91f2e0ecf97b2dd3010b5dbe6199166438a88fad
joseluisreyesm <48143883+joseluisreyesm@users.noreply.github.com>
2022-10-09 14:20:19 -0500

    trae categorias de entrada
    .

---
+++ database/migrations/2022_10_09_191850_create_categorias_de_entradas_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCategoriasDeEntradasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categorias_de_entradas', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre');
+            $table->string('descripcion');
+            $table->boolean('es_activo');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('categorias_de_entradas');
+    }
+}
