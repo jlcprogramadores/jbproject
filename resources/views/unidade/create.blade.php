2fcd9bd328dd26e4bca13f1632ee8dfb044ac16e
CR-MX <31296448+CR-MX@users.noreply.github.com>
2022-10-23 18:52:33 -0500

    Update create.blade.php

--- resources/views/unidade/create.blade.php
+++ resources/views/unidade/create.blade.php
@@ -11,7 +11,7 @@
 
                 <div class="card card-default">
                     <div class="card-header">
-                        <span class="card-title">Crear Unidade</span>
+                        <span class="card-title">Crear Unidad</span>
                     </div>
                     <div class="card-body">
                         <form method="POST" action="{{ route('unidades.store') }}"  role="form" enctype="multipart/form-data">
