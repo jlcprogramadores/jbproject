cccf92ec70bd24c8a17430d0703c1d05c776186f
joseluisreyesm <48143883+joseluisreyesm@users.noreply.github.com>
2022-10-23 19:41:05 -0500

    para que se vea mejor
    .

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -36,11 +36,39 @@
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
-                
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav me-auto">
-
+                        <li class="nav-item dropdown">
+                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                Finanzas
+                            </a>
+                            <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item"  href="{{ url('/') }}">
+                                    {{ __('Egresos') }}
+                                </a>  
+                                <a class="dropdown-item" href="{{ url('/') }}"">
+                                    {{ __('Ingresos') }}
+                                </a> 
+                                
+                                </form>
+                            </div>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                Recursos Humanos
+                            </a>
+                            <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item"  href="{{ url('/') }}"">
+                                    {{ __('Egresos') }}
+                                </a>  
+                                <a class="dropdown-item" href="{{ url('/') }}"">
+                                    {{ __('Ingresos') }}
+                                </a> 
+                                
+                                </form>
+                            </div>
+                        </li>
                         <li class="nav-item dropdown">
                             <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                 Administración
