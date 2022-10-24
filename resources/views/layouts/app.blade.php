380706a0cf7b876aa7c91a8d091634b3ada5b876
CR-MX <31296448+CR-MX@users.noreply.github.com>
2022-10-23 20:04:49 -0500

    cambios

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -31,7 +31,7 @@
         <nav class="navbar navbar-expand-md navbar-light bg-primary shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    jb industrial
+                    <img src="{{ asset('images/JB.png') }}" style="max-width: 15%;">
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
