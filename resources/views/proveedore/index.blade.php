bedfb699a61f2100498292d4ac77636298dbdc62
CR-MX <31296448+CR-MX@users.noreply.github.com>
2022-10-28 13:51:12 -0500

    swal provvedor

--- resources/views/proveedore/index.blade.php
+++ resources/views/proveedore/index.blade.php
@@ -69,7 +69,7 @@
                                                     <a class="btn btn-sm btn-success" href="{{ route('proveedores.edit',$proveedore->id) }}"><i class="fa fa-fw fa-edit"></i> Editar</a>
                                                     @csrf
                                                     @method('DELETE')
-                                                    <button type="submit" class="btn btn-danger btn-sm show_confirms"><i class="fa fa-fw fa-trash"></i> Borrar</button>
+                                                    <button type="submit" class="btn btn-danger btn-sm show_confirm"><i class="fa fa-fw fa-trash"></i> Borrar</button>
                                                 </form>
                                             </td>
                                         </tr>
