fa2ba0b2f266952e6812cc2884b064f7391f286e
CR-MX <31296448+CR-MX@users.noreply.github.com>
2022-10-28 13:40:27 -0500

    correcion

--- resources/views/cliente/form.blade.php
+++ resources/views/cliente/form.blade.php
@@ -53,7 +53,6 @@
     </div>
 </div>
 <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js"></script>
 <script type="text/javascript">
     var i = 0;
     $("#dynamic-ar").click(function () {
