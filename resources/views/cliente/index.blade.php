c3b9d7985330726d687a734e1a0fbeae97f5ec2d
CR-MX <31296448+CR-MX@users.noreply.github.com>
2022-11-19 12:46:09 -0600

    search para todas las tablas

--- resources/views/cliente/index.blade.php
+++ resources/views/cliente/index.blade.php
@@ -118,6 +118,7 @@
     <script src="//cdn.datatables.net/1.12.1/js/jquery.dataTables.min.js"></script>
     <script>
         $(document).ready( function () {
+            $('#table thead tr').clone(true).addClass('filters').appendTo( '#table thead' );
             $('#table').DataTable({
                 responsive:true,
                 autoWidth: false,   
@@ -139,7 +140,35 @@
                     render: function(data, type, full, meta) {    
                         return '<div class="truncate">' + data.split(",").join("<br/>") + '</div>';
                     }
-                }]
+                }],
+                orderCellsTop: true,
+                fixedHeader: true,
+                initComplete: function() {
+                    var api = this.api();
+                    // For each column
+                    api.columns().eq(0).each(function(colIdx) {
+                        // Set the header cell to contain the input element
+                        var cell = $('.filters th').eq($(api.column(colIdx).header()).index());
+                        var title = $(cell).text();
+                        $(cell).html( '<input type="text" placeholder="'+title+'" />' );
+                        // On every keypress in this input
+                        $('input', $('.filters th').eq($(api.column(colIdx).header()).index()) )
+                            .off('keyup change')
+                            .on('keyup change', function (e) {
+                                e.stopPropagation();
+                                // Get the search value
+                                $(this).attr('title', $(this).val());
+                                var regexr = '({search})'; //$(this).parents('th').find('select').val();
+                                var cursorPosition = this.selectionStart;
+                                // Search the column for that value
+                                api
+                                    .column(colIdx)
+                                    .search((this.value != "") ? regexr.replace('{search}', '((('+this.value+')))') : "", this.value != "", this.value == "")
+                                    .draw();
+                                $(this).focus()[0].setSelectionRange(cursorPosition, cursorPosition);
+                            });
+                    });
+                }
             });
         });
         $('.show_confirm').click(function(event) {
--- resources/views/direccione/index.blade.php
+++ resources/views/direccione/index.blade.php
@@ -94,6 +94,7 @@
     <script src="//cdn.datatables.net/1.12.1/js/jquery.dataTables.min.js"></script>
     <script>
         $(document).ready( function () {
+            $('#table thead tr').clone(true).addClass('filters').appendTo( '#table thead' );
             $('#table').DataTable({
                 responsive:true,
                 autoWidth: false,   
@@ -108,6 +109,34 @@
                         "next": "Siguiente",
                         "previous": "Anterior"
                     }
+                },
+                orderCellsTop: true,
+                fixedHeader: true,
+                initComplete: function() {
+                    var api = this.api();
+                    // For each column
+                    api.columns().eq(0).each(function(colIdx) {
+                        // Set the header cell to contain the input element
+                        var cell = $('.filters th').eq($(api.column(colIdx).header()).index());
+                        var title = $(cell).text();
+                        $(cell).html( '<input type="text" placeholder="'+title+'" />' );
+                        // On every keypress in this input
+                        $('input', $('.filters th').eq($(api.column(colIdx).header()).index()) )
+                            .off('keyup change')
+                            .on('keyup change', function (e) {
+                                e.stopPropagation();
+                                // Get the search value
+                                $(this).attr('title', $(this).val());
+                                var regexr = '({search})'; //$(this).parents('th').find('select').val();
+                                var cursorPosition = this.selectionStart;
+                                // Search the column for that value
+                                api
+                                    .column(colIdx)
+                                    .search((this.value != "") ? regexr.replace('{search}', '((('+this.value+')))') : "", this.value != "", this.value == "")
+                                    .draw();
+                                $(this).focus()[0].setSelectionRange(cursorPosition, cursorPosition);
+                            });
+                    });
                 }
             });
         });
