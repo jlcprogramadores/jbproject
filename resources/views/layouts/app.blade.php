5a76b9d00a8cc5b65107caa96d53d9223a34d27d
CR-MX <31296448+CR-MX@users.noreply.github.com>
2022-12-02 19:02:04 -0600

    media query imagen logojb

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -32,7 +32,7 @@
         <nav class="navbar navbar-expand-md navbar-light bg-primary shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('login') }}">
-                    <img src="{{ asset('images/JB.png') }}" style="width: 70px; height: 70px;">
+                    <img src="{{ asset('images/JB.png') }}" class="imgjb">
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
