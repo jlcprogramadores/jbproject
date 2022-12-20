bddef287bea01199a927d1d480bb360409144013
CR-MX <31296448+CR-MX@users.noreply.github.com>
2022-12-20 12:30:28 -0600

    crear permisos y se formateo el codigo

--- resources/views/rol/form.blade.php
+++ resources/views/rol/form.blade.php
@@ -1,39 +1,85 @@
-@if(Auth::check() && Auth::user()->es_activo)
-<div class="box box-info padding-1">
-    <div class="box-body">
- 
-        <div class="form-group">
-            {{ Form::label('Nombre') }}
-            {{ Form::text('name', $rol->name, ['class' => 'form-control' . ($errors->has('name') ? ' is-invalid' : ''), 'placeholder' => 'Nombre']) }}
-            {!! $errors->first('name', '<div class="invalid-feedback">:message</div>') !!}
-        </div>
-            @foreach ($permissions as $permission)
+@if (Auth::check() && Auth::user()->es_activo)
+    <div class="box box-info padding-1">
+        <div class="box-body">
 
-
-                <div>
-                    <label>
-                        <?php 
-                           $mostar= substr($permission->nomenclatura, 5); 
-                           dd($mostar);
+            <div class="form-group">
+                {{ Form::label('Nombre') }}
+                {{ Form::text('name', $rol->name, ['class' => 'form-control' . ($errors->has('name') ? ' is-invalid' : ''), 'placeholder' => 'Nombre']) }}
+                {!! $errors->first('name', '<div class="invalid-feedback">:message</div>') !!}
+            </div>
+            <?php
+            // parte de los headers
+            $menuAnterior = null;
+            $contador = 0;
+            $titulos = ['Finanzas', 'Recursos Humanos', 'Administración'];
+            $indicadorEnPM = '00';
+            ?>
+            <br>
+            <div class="container">
+                <div class="row">
+                    @foreach ($permissions as $permission)
+                        <?php
+                        //  parte de los headers
+                        $menu = substr($permission->nomenclatura, 3, 2);
+                        $menuEsIgual = $menuAnterior == $menu;
+                        $menuAnterior = $menu;
+                        // parte submenus
+                        $subMenu = substr($permission->nomenclatura, 15, 2);
+                        $subMenuEsIgual = $indicadorEnPM == $subMenu;
                         ?>
-                        {{dd($mostar)}}
-                        <div class="card bg-light mb-3" style="max-width: 18rem;">
-                            <div class="card-header">Header</div>
-                            <div class="card-body">
-                              <h5 class="card-title">Light card title</h5>
-                              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        {{-- Cuando es el primero --}}
+                        @if ($menuEsIgual == false && $contador == 0)
+                            <div class="col-sm" data-bs-spy="scroll">
+                                <li class="list-group-item list-group-item-primary">
+                                    <div class="d-flex justify-content-between">
+                                        <div>
+                                            {!! Form::checkbox('permissions[]', $permission->id, null, ['class' => 'mr-1']) !!}
+                                            {{ $permission->description }}
+                                        </div>
+                                        <div class="fw-bold"> {{ $titulos[$contador] }}</div>
+                                    </div>
+                                </li>
+                                <ul class="list-group">
+
+                                    <?php $contador++; ?>
+                                @elseif($menuEsIgual == true)
+                                    @if ($subMenuEsIgual)
+                                        <li class="list-group-item list-group-item-secondary">
+                                            {!! Form::checkbox('permissions[]', $permission->id, null, ['class' => 'mr-1']) !!}
+                                            {{ $permission->description }}
+                                        </li>
+                                    @else
+                                        <li class="list-group-item">
+                                            {!! Form::checkbox('permissions[]', $permission->id, null, ['class' => 'mr-1']) !!}
+                                            {{ $permission->description }}
+                                        </li>
+                                    @endif
+                                @elseif($menuEsIgual == false)
+                                </ul>
                             </div>
-                          </div>
-                          
-                        {!! Form::checkbox('permissions[]',$permission->id,null,['class' => 'mr-1']) !!}
-                        {{$permission->description}}
-                    </label>
+                            <div class="col-sm" data-bs-spy="scroll">
+
+                                <li class="list-group-item list-group-item-primary">
+                                    <div class="d-flex justify-content-between">
+                                        <div>
+                                            {!! Form::checkbox('permissions[]', $permission->id, null, ['class' => 'mr-1']) !!}
+                                            {{ $permission->description }}
+                                        </div>
+                                        <div class="fw-bold">{{ $titulos[$contador] }}</div>
+                                    </div>
+                                </li>
+                                <ul class="list-group">
+
+                                    <?php $contador++; ?>
+                        @endif
+                    @endforeach
+                    </ul>
                 </div>
-            @endforeach
+            </div>
         </div>
-    <div class="box-footer mt20">
-        <a class="btn btn-danger" href="{{ route('roles.index') }}"> Atrás</a>
-        <button type="submit" id="btn-aceptar" onclick="myFunction();" class="btn btn-primary">Aceptar</button>
+        <div class="box-footer mt20">
+            <a class="btn btn-danger" href="{{ route('roles.index') }}"> Atrás</a>
+            <button type="submit" id="btn-aceptar" onclick="myFunction();" class="btn btn-primary">Aceptar</button>
+        </div>
     </div>
-</div>
 @endif
--- resources/views/rol/partials/form.blade.php
+++ resources/views/rol/partials/form.blade.php
@@ -1,105 +1,93 @@
-@if(Auth::check() && Auth::user()->es_activo)
-<div class="box box-info padding-1">
-    <div class="box-body">
-        <div class="form-group">
-            {{ Form::label('Nombre') }}
-            {{ Form::text('name', $rol->name, ['class' => 'form-control' . ($errors->has('name') ? ' is-invalid' : ''), 'placeholder' => 'Name']) }}
-            {!! $errors->first('name', '<div class="invalid-feedback">:message</div>') !!}
-        </div>
-
-            <?php 
-                // parte de los headers
-                $menuAnterior = null;
-                $contador = 0;
-                $titulos = array('Finanzas', 'Recursos Humanos', 'Administración');
-                $indicadorEnPM = '00';
-            ?>
-    <br>
-    <div class="container">
-        <div class="row">
-
-
-            
-            @foreach ($permissions as $permission)
-                <?php 
-                    //  parte de los headers 
-                    $menu = substr($permission->nomenclatura,3, 2); 
-                    $menuEsIgual = $menuAnterior == $menu;
-                    $menuAnterior = $menu;
-                    // parte submenus
-                    $subMenu = substr($permission->nomenclatura,15, 2); 
-                    $subMenuEsIgual = $indicadorEnPM == $subMenu;
-                ?>
-                {{-- Cuando es el primero --}}
-                @if ($menuEsIgual == false && $contador == 0)
-                    <div class="col-sm"  data-bs-spy="scroll">
-                    <li class="list-group-item list-group-item-primary"> 
-                        <div class="d-flex justify-content-between">
-                            <div>
-                                {!! Form::checkbox('permissions[]',$permission->id,null,['class' => 'mr-1']) !!}
-                                {{$permission->description}}
-                            </div>
-                            <div class="fw-bold"> {{$titulos[$contador]}}</div> 
-                        </div>
-                    </li>
-                    <ul class="list-group">
-
-                    <?php $contador++?>
-    
-                @elseif($menuEsIgual == true)
-                    @if ($subMenuEsIgual)
-                        <li class="list-group-item list-group-item-secondary">
-                            {!! Form::checkbox('permissions[]',$permission->id,null,['class' => 'mr-1']) !!}
-                            {{$permission->description}}
-                        </li>                    
-                    @else
-                        <li class="list-group-item">
-                            {!! Form::checkbox('permissions[]',$permission->id,null,['class' => 'mr-1']) !!}
-                            {{$permission->description}}
-                        </li>
-                    @endif
-    
-                @elseif($menuEsIgual == false)
-                    </ul>
-                    </div>
-                    <div class="col-sm"  data-bs-spy="scroll">
-    
-                    <li class="list-group-item list-group-item-primary"> 
-                        <div class="d-flex justify-content-between">
-                            <div >
-                                {!! Form::checkbox('permissions[]',$permission->id,null,['class' => 'mr-1']) !!}
-                                {{$permission->description}}
-                            </div>
-                            <div class="fw-bold" >{{$titulos[$contador]}}</div> 
-                        </div>
-                    </li>
-                    <ul class="list-group">
-
-                    <?php $contador++?>
-                
-                @endif
-    
-                    
-            @endforeach
-            </ul>
+@if (Auth::check() && Auth::user()->es_activo)
+    <div class="box box-info padding-1">
+        <div class="box-body">
+            <div class="form-group">
+                {{ Form::label('Nombre') }}
+                {{ Form::text('name', $rol->name, ['class' => 'form-control' . ($errors->has('name') ? ' is-invalid' : ''), 'placeholder' => 'Name']) }}
+                {!! $errors->first('name', '<div class="invalid-feedback">:message</div>') !!}
             </div>
 
+            <?php
+            // parte de los headers
+            $menuAnterior = null;
+            $contador = 0;
+            $titulos = ['Finanzas', 'Recursos Humanos', 'Administración'];
+            $indicadorEnPM = '00';
+            ?>
+            <br>
+            <div class="container">
+                <div class="row">
 
 
 
+                    @foreach ($permissions as $permission)
+                        <?php
+                        //  parte de los headers
+                        $menu = substr($permission->nomenclatura, 3, 2);
+                        $menuEsIgual = $menuAnterior == $menu;
+                        $menuAnterior = $menu;
+                        // parte submenus
+                        $subMenu = substr($permission->nomenclatura, 15, 2);
+                        $subMenuEsIgual = $indicadorEnPM == $subMenu;
+                        ?>
+                        {{-- Cuando es el primero --}}
+                        @if ($menuEsIgual == false && $contador == 0)
+                            <div class="col-sm" data-bs-spy="scroll">
+                                <li class="list-group-item list-group-item-primary">
+                                    <div class="d-flex justify-content-between">
+                                        <div>
+                                            {!! Form::checkbox('permissions[]', $permission->id, null, ['class' => 'mr-1']) !!}
+                                            {{ $permission->description }}
+                                        </div>
+                                        <div class="fw-bold"> {{ $titulos[$contador] }}</div>
+                                    </div>
+                                </li>
+                                <ul class="list-group">
+
+                                    <?php $contador++; ?>
+                                @elseif($menuEsIgual == true)
+                                    @if ($subMenuEsIgual)
+                                        <li class="list-group-item list-group-item-secondary">
+                                            {!! Form::checkbox('permissions[]', $permission->id, null, ['class' => 'mr-1']) !!}
+                                            {{ $permission->description }}
+                                        </li>
+                                    @else
+                                        <li class="list-group-item">
+                                            {!! Form::checkbox('permissions[]', $permission->id, null, ['class' => 'mr-1']) !!}
+                                            {{ $permission->description }}
+                                        </li>
+                                    @endif
+                                @elseif($menuEsIgual == false)
+                                </ul>
+                            </div>
+                            <div class="col-sm" data-bs-spy="scroll">
+
+                                <li class="list-group-item list-group-item-primary">
+                                    <div class="d-flex justify-content-between">
+                                        <div>
+                                            {!! Form::checkbox('permissions[]', $permission->id, null, ['class' => 'mr-1']) !!}
+                                            {{ $permission->description }}
+                                        </div>
+                                        <div class="fw-bold">{{ $titulos[$contador] }}</div>
+                                    </div>
+                                </li>
+                                <ul class="list-group">
+
+                                    <?php $contador++; ?>
+                        @endif
+                    @endforeach
+                    </ul>
+                </div>
+
+
+
+
+            </div>
         </div>
-    </div>
-
-            
-
-
     </div>
     <div class="box-footer mt20">
         <a class="btn btn-danger" href="{{ route('roles.index') }}"> Atrás</a>
         <button type="submit" id="btn-aceptar" onclick="myFunction();" class="btn btn-primary">Aceptar</button>
     </div>
-</div>
+    </div>
 @endif
-
-
-
