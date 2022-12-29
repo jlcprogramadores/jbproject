9279d30032f69646a9b5ed6f875b44a57ffc273e
CR-MX <31296448+CR-MX@users.noreply.github.com>
2022-12-29 12:11:43 -0600

    swal

--- resources/views/empleado-expediente/indexEmpExpedientes.blade.php
+++ resources/views/empleado-expediente/indexEmpExpedientes.blade.php
@@ -55,7 +55,7 @@
                                                     <a class="btn btn-sm btn-success" href="{{ route('empleado-expedientes.edit',$item->id) }}"><i class="fa fa-fw fa-edit"></i> Editar</a>
                                                     @csrf
                                                     @method('DELETE')
-                                                    <button type="submit" class="btn btn-danger btn-sm"><i class="fa fa-fw fa-trash"></i> Borrar</button>
+                                                    <button type="submit" class="btn btn-danger btn-sm show_confirm"><i class="fa fa-fw fa-trash"></i> Borrar</button>
                                                 </form>
                                             </td>
                                         </tr>
