3d90496437e9e31693ec5a36a100837827fa53ce
CR-MX <31296448+CR-MX@users.noreply.github.com>
2023-01-04 15:43:05 -0600

    index y form de los candidatos

--- resources/views/mina/form.blade.php
+++ resources/views/mina/form.blade.php
@@ -1,24 +1,32 @@
 <div class="box box-info padding-1">
     <div class="box-body">
-        
-        <div class="form-group">
-            {{ Form::label('nombre') }}
-            {{ Form::text('nombre', $mina->nombre, ['class' => 'form-control' . ($errors->has('nombre') ? ' is-invalid' : ''), 'placeholder' => 'Nombre']) }}
-            {!! $errors->first('nombre', '<div class="invalid-feedback">:message</div>') !!}
+        <div class="container">
+            <div class="row">
+                <div class="form-group">
+                    {{ Form::label('nombre') }}
+                    {{ Form::text('nombre', $mina->nombre, ['class' => 'form-control' . ($errors->has('nombre') ? ' is-invalid' : ''), 'placeholder' => 'Nombre']) }}
+                    {!! $errors->first('nombre', '<div class="invalid-feedback">:message</div>') !!}
+                </div>
+                <div class="form-group">
+                    {{ Form::label('descripcion') }}
+                    {{ Form::text('descripcion', $mina->descripcion, ['class' => 'form-control' . ($errors->has('descripcion') ? ' is-invalid' : ''), 'placeholder' => 'Descripcion']) }}
+                    {!! $errors->first('descripcion', '<div class="invalid-feedback">:message</div>') !!}
+                </div>
+                <div class="form-group d-none">
+                    {{ Form::label('usuario_edito') }}
+                    {{ Form::text('usuario_edito', Auth::user()->name, ['class' => 'form-control' . ($errors->has('usuario_edito') ? ' is-invalid' : ''), 'placeholder' => 'Usuario Edito']) }}
+                    {!! $errors->first('usuario_edito', '<div class="invalid-feedback">:message</div>') !!}
+                </div>
+            </div>
+            <br>
+            <div class="row">
+                <div class="row justify-content-md-center">
+                    <br>
+                    <a href="{{ route('candidatos.index') }}" class="btn btn-danger col col-lg-3">{{ __('Cancelar')}}</a>
+                    <br>
+                    <button type="submit" id="btn-aceptar" onclick="myFunction();" class="btn btn-primary col col-lg-3">Aceptar</button>
+                </div>
+            </div>
         </div>
-        <div class="form-group">
-            {{ Form::label('descripcion') }}
-            {{ Form::text('descripcion', $mina->descripcion, ['class' => 'form-control' . ($errors->has('descripcion') ? ' is-invalid' : ''), 'placeholder' => 'Descripcion']) }}
-            {!! $errors->first('descripcion', '<div class="invalid-feedback">:message</div>') !!}
-        </div>
-        <div class="form-group">
-            {{ Form::label('usuario_edito') }}
-            {{ Form::text('usuario_edito', $mina->usuario_edito, ['class' => 'form-control' . ($errors->has('usuario_edito') ? ' is-invalid' : ''), 'placeholder' => 'Usuario Edito']) }}
-            {!! $errors->first('usuario_edito', '<div class="invalid-feedback">:message</div>') !!}
-        </div>
-
-    </div>
-    <div class="box-footer mt20">
-        <button type="submit" class="btn btn-primary">Submit</button>
     </div>
 </div>
