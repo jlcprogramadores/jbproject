6cb3755305f2b24a69c43270c79780fde191e751
CR-MX <31296448+CR-MX@users.noreply.github.com>
2023-01-09 11:27:19 -0600

    semaforo de fechas limite de expediente

--- resources/views/empleado-expediente/index.blade.php
+++ resources/views/empleado-expediente/index.blade.php
@@ -43,7 +43,59 @@
                                         <tr>
                                             <td>{{ ++$i }}</td>
 											<td>{{ $empleado->nombre }}</td>
-											<td>{{$empleado->fecha_limite_expediente ? Carbon\Carbon::parse($empleado->fecha_limite_expediente)->format('Y-m-d') : '' }}</td>
+
+
+                                            @if ($empleado->fecha_limite_expediente)
+                                                <?php 
+                                                    $fechaActual = Carbon\Carbon::createFromFormat('Y-m-d', date('Y-m-d'));
+                                                    $fechaLimite = Carbon\Carbon::parse( $empleado->fecha_limite_expediente)->format('Y-m-d');
+                                                    
+                                                    $shippingDate = Carbon\Carbon::createFromFormat('Y-m-d',$fechaLimite );
+                                                    $diferencia_en_dias = $fechaActual->diffInDays($shippingDate);
+                                                    //  si ya se paso el dia se concidera vencido
+                                                    $estaVencido = false;
+
+                                                ?>
+                                                {{-- mayor iguala a 4 verde
+                                                    = 3 marillo
+                                                    si es menor rojo  --}}
+                                                @if ($fechaActual > $fechaLimite && $diferencia_en_dias != 0)
+                                                <td>
+                                                    <p class="bg-danger text-white" >
+                                                        Vencido
+                                                        <br>
+                                                        {{Carbon\Carbon::parse($empleado->fecha_limite_expediente)->format('Y-m-d')}}
+                                                    </p>
+                                                </td>
+                                                @else     
+                                                    <td> 
+                                                        @if ($diferencia_en_dias >= 4)
+                                                            <p class=" bg-success text-white" >
+                                                                Restan {{$diferencia_en_dias}} días
+                                                                <br>
+                                                                {{Carbon\Carbon::parse($empleado->fecha_limite_expediente)->format('Y-m-d')}}
+                                                            </p>
+                                                        @elseif($diferencia_en_dias == 3)
+                                                            <p class="bg-warning text-dark" >
+                                                                Restan {{$diferencia_en_dias}} días
+                                                                <br>
+                                                                {{Carbon\Carbon::parse($empleado->fecha_limite_expediente)->format('Y-m-d')}}
+
+                                                            </p>
+                                                        @elseif($diferencia_en_dias >= -1)
+                                                            <p class="bg-danger text-white" >
+                                                                Restan {{$diferencia_en_dias}} días
+                                                                <br>
+                                                                {{Carbon\Carbon::parse($empleado->fecha_limite_expediente)->format('Y-m-d')}}
+                                                            </p>
+                                                        @endif
+
+                                                    </td>
+                                                @endif
+                                            @else
+                                                <td></td>
+                                            @endif
+
 											<td>{{ $empleado->usuario_edito }} <br/> {{ $empleado->updated_at }}</td>  
                                             <td>
                                                 <form action="{{ route('empleado-expedientes.destroy',$empleado->id) }}" method="POST">
