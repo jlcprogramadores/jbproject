b54fef188e77dcd976b6ca70a31f56a9dd7c9f40
joseluisreyesm <48143883+joseluisreyesm@users.noreply.github.com>
2023-01-12 16:19:53 -0600

    SOLO FALTA MOVIL PARA GRUPOS
    .

--- resources/views/grupo/form.blade.php
+++ resources/views/grupo/form.blade.php
@@ -27,26 +27,8 @@
                     <th>Acción</th>
                 </tr>
                 <tr>
-                    {{-- <td>
-                        <select  id="empleado_id" type="text" name="empleado[0][empleado_id]" class="form-control">
-                            <option value=''>Selecciona Empleado</option>
-                            @foreach ($empleados as $key => $value) 
-                            <option value='{{$key}}'>{{$value}}</option>
-                            @endforeach
-                        </select>
-                    </td> --}}
-                    
-                    
-                    {{-- <td>
-                        <select  id="puesto_id" type="text" name="empleado[0][puesto_id]" class="form-control">
-                        <option value=''>Selecciona Puesto</option>
-                        @foreach ($puestos as $key => $value) 
-                        <option value='{{$key}}'>{{$value}}</option>
-                        @endforeach
-                    </select>
-                </td> --}}
-                    <td><select id="empleado_id" type="text" name="empleado[0][empleado_id]" class="form-control"></select></td>
-                    <td><select id="puesto_id" type="text" name="empleado[0][puesto_id]"  class="my-select"></select></td>
+                    <td><select id="empleado_id" type="text" style="width: auto;" name="empleado[0][empleado_id]" class="form-control" required/>></select></td> 
+                    <td><select id="puesto_id" type="text" style="width: auto;" name="empleado[0][puesto_id]" class="form-control" required/>></select></td> 
                     <td><input id="salario" type="number"  step="any" name="empleado[0][salario]" class="form-control"/></td>
                     <td><button type="button" name="add" id="dynamic-ar" class="btn btn-outline-primary">Añadir</button></td>
                 </tr>
@@ -63,14 +45,16 @@
 @push('scripts')
 <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.13/js/select2.min.js" integrity="sha512-2ImtlRlf2VVmiGZsjm9bEyhjGW4dU7B6TNwh/hx/iSByxNENtj3WVE6o/9Lj4TJeVXPi4bnOIMXFIJJAeufa0A==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 <script>
-     var i = 0;
+    var i = 0;
+    var marcador = 0;
         $("#dynamic-ar").click(function () {
             ++i;
+            ++marcador;
             $("#dynamicAddRemove").append(
                 
                 '<tr>'+
-                    '<td><select id="empleado_id" type="text" name="empleado['+i+'][empleado_id]" class="form-control" required/></select></td>'+
-                    '<td><select id="puesto_id" type="text" name="empleado['+i+'][puesto_id]"  class="my-select-'+i+'" required /></select></td>'+
+                    '<td><select id="empleado_id'+i+'" type="text" name="empleado['+i+'][empleado_id]" style="width: auto;" class="form-control" required/></select></td>'+
+                    '<td><select id="puesto_id'+i+'" type="text" name="empleado['+i+'][puesto_id]" style="width: auto;" class="form-control" required /></select></td>'+
                     '<td><input type="number" step="any" name="empleado['+i+'][salario]" class="form-control" required /></td>'+
                     '<td><button type="button" class="btn btn-outline-danger remove-input-field">Borrar</button></td>'+
                 '</tr>'
@@ -87,32 +71,41 @@
         });
         
         function llenar() {
-            var empleado_id = <?php echo json_encode($empleados)?>;
-            var select2 = document.querySelectorAll('[id=empleado_id]');            
-            select2.forEach(function (element) {
-                var elementoZ = element;
-                elementoZ.options.length = 0;
-                for(index in empleado_id) {
-                    elementoZ.options[elementoZ.options.length] = new Option(empleado_id[index], index);
-                }
-            });
-
-            
-            var puesto_id = <?php echo json_encode($puestos)?>;
-            var select1 = document.querySelectorAll('[id=puesto_id]');
-            select1.forEach(function (element) {
-                var elementoX = element;
-                console.log(elementoX);
-                elementoX.options.length = 0;
-                for(index in puesto_id) {
-                    elementoX.options[elementoX.options.length] = new Option(puesto_id[index], index);
-                }
-            });
+            for (let marcadorIndex = 1; marcadorIndex < marcador+1; marcadorIndex++) {
+                var empleado_id = <?php echo json_encode($empleados)?>;
+                var selectEmpleado = document.querySelectorAll('[id=empleado_id'+marcadorIndex+']');
+                selectEmpleado.forEach(function (element) {
+                    var elementoEmpleado = element;
+                    elementoEmpleado.options.length = 0;
+                    for(index in empleado_id) {
+                        elementoEmpleado.options[elementoEmpleado.options.length] = new Option(empleado_id[index], index);
+                        var cadena = '#empleado_id'+marcadorIndex;
+                        $('#empleado_id').select2();
+                        $(cadena).select2();
+                    }
+                });
+            }
+   
+            for (let marcadorIndex = 1; marcadorIndex < marcador+1; marcadorIndex++) {
+                var puesto_id = <?php echo json_encode($puestos)?>;
+                var selectPuesto = document.querySelectorAll('[id=puesto_id'+marcadorIndex+']');
+                selectPuesto.forEach(function (element) {
+                    var elementoPuesto = element;
+                    elementoPuesto.options.length = 0;
+                    for(index in puesto_id) {
+                        elementoPuesto.options[elementoPuesto.options.length] = new Option(puesto_id[index], index);
+                        var cadena = '#puesto_id'+marcadorIndex;
+                        $('#puesto_id').select2();
+                        $(cadena).select2();
+                    }
+                });
+            }
             
         }
 
         $(document).on('click', '.remove-input-field', function () {
             --i;
+            --marcador;
             $(this).parents('tr').remove();
             if(i == 0){
                 comprobante = document.getElementById('comprobante');
@@ -123,7 +116,26 @@
         });
 
     function mostrarDiv() {
-        llenar();
+        var empleado_id = <?php echo json_encode($empleados)?>;
+        var selectEmpleado = document.querySelectorAll('[id=empleado_id]');
+        selectEmpleado.forEach(function (element) {
+            var elementoEmpleado = element;
+            for(index in empleado_id) {
+                elementoEmpleado.options[elementoEmpleado.options.length] = new Option(empleado_id[index], index);
+                $('#empleado_id').select2();
+            }
+        });
+
+        var puesto_id = <?php echo json_encode($puestos)?>;
+        var selectPuesto = document.querySelectorAll('[id=puesto_id]');
+        selectPuesto.forEach(function (element) {
+            var elementoPuesto = element;
+            for(index in puesto_id) {
+                elementoPuesto.options[elementoPuesto.options.length] = new Option(puesto_id[index], index);
+                $('#puesto_id').select2();
+            }
+        });
+                
         var apartadoEmpleado = document.getElementById('apartadoEmpleado');
         if (apartadoEmpleado.style.display === "none") {
             
@@ -138,7 +150,5 @@
             document.getElementById('salario').required = false;
         }
     }
-
-
 </script>
 @endpush
