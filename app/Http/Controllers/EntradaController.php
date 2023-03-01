d1753702e414c40f5995ad43f86cb4be08604c19
CR-MX <ciat117@gmail.com>
2023-03-01 11:52:03 -0600

    cambios

--- app/Http/Controllers/EntradaController.php
+++ app/Http/Controllers/EntradaController.php
@@ -3,11 +3,15 @@
 namespace App\Http\Controllers;
 
 use App\Models\Entrada;
+use App\Models\Salida;
+use App\Models\Finanza;
 use Illuminate\Http\Request;
 use App\Models\Cliente;
 use App\Models\TipoDeIngreso;
 use App\Models\CategoriasDeEntrada;
 use App\Models\Proyecto;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\DB;
 // use Excel;
 // use Importer;
 use Rap2hpoutre\FastExcel\FastExcel;
@@ -125,22 +129,82 @@ class EntradaController extends Controller
     
     public function exel(Request $request)
     {
-        // $file = 
+        // datos de la cabezera del archivo
+        $no= "NO.";
+        $fechaEntrada= "FECHA ENTRADA"; 
+        $fechaSalida= "FECHA SALIDA"; 
+        $vence= "VENCE"; 
+        $fechaVencimiento= "FECHA VENCIMIENTO"; 
+        $dias= "DIAS "; 
+        $estado= "ESTADO"; 
+        $tipoEyS= "TIPO E&S"; 
+        $familia= "FAMILIA"; 
+        $categoria= "CATEGORIA"; 
+        $razon= "RAZON SOCIAL"; 
+        $proyecto= "PROYECTO  "; 
+        $descripcion= "DESCRIPCION"; 
+        $factura= "FACTURA  O FOLIO";
+        $proveedor= "PROVEEDOR ";
+        $cantidad= "CANTIDAD"; 
+        $unidad= "UNIDAD"; 
+        $costoUnitario= "COSTO UNITARIO"; 
+        $subTotal= "SUBTOTAL TOTAL MXN"; 
+        $iva= "IVA"; 
+        $total= "TOTAL       \$MXN$"; 
+        $monto= "MONTO A PAGAR";
+        $fechaDePago= "FECHA DE PAGO"; 
+        $metodoDePago= "METODO DE PAGO"; 
+        $estatus= "$ ESTATUS $"; 
+        $entregadoA= "ENTREGADO MATERIAL A "; 
+        $comentarios= "COMENTARIOS";
+        // datos generales
+        $usuario_edito = 'sistema';
+
+
+        $dt = Carbon::now();
+        $dateNow = $dt->toDateTimeString();
         $path = $request->file('file')->getRealPath();
         $collection = (new FastExcel)->import($path);
-        dd($collection);
-        // $excel = Importer::make('Excel');
-        // $excel->load($path);
-        // $collection = $excel->getCollection();
         // dd($collection);
-        // // $datos = Excel::load($path )->get();
-        // dd($datos->count());
-        // $datos = $datos->toArray();
-        // for ($i=0; $i < count($datos) ; $i++) { 
-        //     $datosImportar[] = $datos [$i];
-        // }
 
-        // dd($datosImportar);
+
+
+        foreach($collection as $file){
+            // creacion de entrada o salida
+            if($file[$tipoEyS] == "SALIDA" ){ //salida
+                $dbProveedor = DB::table('proveedores')->where('nombre',DB::raw("'".$file[$proveedor]."'"))->get();
+                $datosSalida =[
+                    'proveedor_id' => isset($dbProveedor->id)? $dbProveedor->id : null,
+                    'usuario_edito' => $usuario_edito,
+                    // 'comprobante' => ,
+                    'enviado' => 0,
+                    'created_at' => $dateNow,
+                    'updated_at' => $dateNow,
+                ];
+                // dd($datosSalida);
+                // $salida = Salida::create($datosSalida);
+            }else{ //entrada
+                $dbCliente = DB::table('clientes')->where('nombre',DB::raw("'".$file[$proveedor]."'"))->get();
+                $dbProyecto = DB::table('proyectos')->where('nombre',DB::raw("'".$file[$proyecto]."'"))->get();
+
+                $datosEntrada =[
+                    'cliente_id' => isset($dbCliente->id) ? $dbCliente->id : null,
+                    'tipodeingreso_id' => 0,
+                    'categorias_de_entrada_id' => 0,
+                    'proyecto_id' => isset($dbProyecto->id) ? $dbProyecto->id : null,
+                    'usuario_edito' => $usuario_edito,
+                    'created_at' => $dateNow,
+                    'updated_at' => $dateNow,
+                ];
+                dd($datosEntrada);
+                // $entrada = Entrada::create($datosEntrada);
+            }
+            dd($file['TIPO E&S']);
+
+            // creacion de finanza
+        }
+        dd();
+
     }
 
 
