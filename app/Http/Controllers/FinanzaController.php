8624e3fcdbe77d200dfeb938eb361133d1ed1203
joseluisreyesm <48143883+joseluisreyesm@users.noreply.github.com>
2023-03-05 16:16:32 -0600

    prueba 5.
    .

--- app/Http/Controllers/FinanzaController.php
+++ app/Http/Controllers/FinanzaController.php
@@ -39,7 +39,7 @@ class FinanzaController extends Controller
      */
     public function index()
     {
-        $finanzas = Finanza::paginate();
+        $finanzas = Finanza::paginate(200);
         return view('finanza.index', compact('finanzas'))
             ->with('i', (request()->input('page', 1) - 1) * $finanzas->perPage());
     }
