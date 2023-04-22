877b093ceba99009f041a867c7f6931e205b6af3
CR-MX <ciat117@gmail.com>
2023-04-22 15:06:25 -0600

    .

--- resources/views/producto/index.blade.php
+++ resources/views/producto/index.blade.php
@@ -16,7 +16,7 @@
 
                              <div class="float-right">
                                 <a href="{{ route('productos.create') }}" class="btn btn-primary btn-sm float-right"  data-placement="left">
-                                  {{ __('Create New') }}
+                                  {{ __('Crear Nuevo') }}
                                 </a>
                               </div>
                         </div>
