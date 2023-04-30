76b5458b036c85fd14f35fe6e42efe4b5c413c28
CR-MX <ciat117@gmail.com>
2023-04-30 11:04:33 -0600

    form producto

--- resources/views/producto/form.blade.php
+++ resources/views/producto/form.blade.php
@@ -3,42 +3,48 @@
     <div class="box-body">
         <div class="container">
             <div class="row">
-                <div class="form-group">
-                    {{ Form::label('codigo') }}
+                <div class="col-sm-2 form-group">
+                    {{ Form::label('código') }}
                     {{ Form::text('codigo', $producto->codigo, ['class' => 'form-control' . ($errors->has('codigo') ? ' is-invalid' : ''), 'placeholder' => 'Codigo']) }}
                     {!! $errors->first('codigo', '<div class="invalid-feedback">:message</div>') !!}
                 </div>
-                <div class="form-group">
-                    {{ Form::label('descripcion') }}
+                <div class="col-sm-10 form-group">
+                    {{ Form::label('descripción') }}
                     {{ Form::text('descripcion', $producto->descripcion, ['class' => 'form-control' . ($errors->has('descripcion') ? ' is-invalid' : ''), 'placeholder' => 'Descripcion']) }}
                     {!! $errors->first('descripcion', '<div class="invalid-feedback">:message</div>') !!}
                 </div>
-                <div class="form-group">
+            </div>
+            <div class="row">
+                <div class="col-sm form-group">
                     {{ Form::label('marca') }}
                     {{ Form::text('marca', $producto->marca, ['class' => 'form-control' . ($errors->has('marca') ? ' is-invalid' : ''), 'placeholder' => 'Marca']) }}
                     {!! $errors->first('marca', '<div class="invalid-feedback">:message</div>') !!}
                 </div>
-                <div class="form-group">
+                <div class="col-sm form-group">
                     {{ Form::label('modelo') }}
                     {{ Form::text('modelo', $producto->modelo, ['class' => 'form-control' . ($errors->has('modelo') ? ' is-invalid' : ''), 'placeholder' => 'Modelo']) }}
                     {!! $errors->first('modelo', '<div class="invalid-feedback">:message</div>') !!}
                 </div>
-                <div class="form-group">
+            </div>
+            <div class="row">
+                
+                
+                <div class="col-sm form-group">
                     {{ Form::label('precio_unitario') }}
                     {{ Form::number('precio_unitario', $producto->precio_unitario, ['class' => 'form-control' . ($errors->has('precio_unitario') ? ' is-invalid' : ''),'min'=>'0', 'step'=>'any','placeholder' => 'Precio Unitario']) }}
                     {!! $errors->first('precio_unitario', '<div class="invalid-feedback">:message</div>') !!}
                 </div>
-                <div class="form-group">
+                <div class="col-sm form-group">
                     {{ Form::label('minimo') }}
                     {{ Form::number('minimo', $producto->minimo, ['class' => 'form-control' . ($errors->has('minimo') ? ' is-invalid' : ''),'min'=>'0', 'placeholder' => 'Minimo']) }}
                     {!! $errors->first('minimo', '<div class="invalid-feedback">:message</div>') !!}
                 </div>
-                <div class="form-group">
+                <div class="col-sm form-group">
                     {{ Form::label('maximo') }}
                     {{ Form::number('maximo', $producto->maximo, ['class' => 'form-control' . ($errors->has('maximo') ? ' is-invalid' : ''),'min'=>'0', 'placeholder' => 'Maximo']) }}
                     {!! $errors->first('maximo', '<div class="invalid-feedback">:message</div>') !!}
                 </div>
-                <div class="form-group d-none">
+                <div class="col-sm form-group d-none">
                     {{ Form::label('usuario_edito') }}
                     {{ Form::text('usuario_edito', Auth::user()->name, ['class' => 'form-control' . ($errors->has('usuario_edito') ? ' is-invalid' : ''), 'placeholder' => 'Usuario Edito']) }}
                     {!! $errors->first('usuario_edito', '<div class="invalid-feedback">:message</div>') !!}
