c4907d46ce5771d57816a589678b8ce6789b2b5e
CR-MX <ciat117@gmail.com>
2023-06-26 18:43:09 -0600

    feat: ahora se filtra toda la tabla finazas con livewire

--- app/Http/Livewire/FinanzaTabla.php
+++ app/Http/Livewire/FinanzaTabla.php
@@ -50,13 +50,14 @@ class FinanzaTabla extends Component
     public function render()
 {
     $finanzas = Finanza::select(
+            'finanzas.id',
             'finanzas.no',
             DB::raw("DATE_FORMAT(finanzas.fecha_entrada, '%Y-%m-%d') as fecha_entrada"),
             DB::raw("DATE_FORMAT(finanzas.fecha_salida, '%Y-%m-%d') as fecha_salida"),
             'finanzas.vence',
             DB::raw("@fven := DATE_FORMAT(DATE_ADD(finanzas.fecha_salida, INTERVAL finanzas.vence DAY), '%Y-%m-%d') as fecha_vencimiento"),
-            DB::raw("@dias := DATEDIFF(@fven, NOW()) as dias"),
-            DB::raw("if(@dias <= 0, 'Vencido', 'Por Vencer') as estado"),
+            DB::raw("@dias := DATEDIFF(DATE_FORMAT(DATE_ADD(finanzas.fecha_salida, INTERVAL finanzas.vence DAY), '%Y-%m-%d'), NOW()) as dias"),
+            DB::raw("if(DATEDIFF(DATE_FORMAT(DATE_ADD(finanzas.fecha_salida, INTERVAL finanzas.vence DAY), '%Y-%m-%d'), NOW())<= 0, 'Vencido', 'Por Vencer') as estado"),
             DB::raw("if(finanzas.entradas_id is not NULL, 'Egreso', 'Ingreso') as tipo"),
             DB::raw("CONCAT(IF(familias.nombre IS NULL, '', CONCAT('F:', familias.nombre,', ')), IF(categorias_familias.nombre IS NULL, '', CONCAT('C:', categorias_familias.nombre))) AS fam_cat"),
             DB::raw("if(finanzas.salidas_id is not NULL, proveedores.razon_social, clientes.razon_social) as razon_social"),
@@ -64,19 +65,17 @@ class FinanzaTabla extends Component
             'finanzas.descripcion as descripcion',
 
             DB::raw("(SELECT COALESCE(GROUP_CONCAT(fac.referencia_factura),if(finanzas.entradas_id is not NULL, 'No Facturado', 'No Recibido')) FROM facturas as fac WHERE fac.finanza_id = finanzas.id) as fac_o_fol"),
-            'finanzas.id',
-            'proveedores.nombre as pro_nombre',
-            'clientes.nombre as cli_nombre',
+            DB::raw("if(finanzas.salidas_id is not NULL, proveedores.nombre, clientes.nombre) as provedor_cliente"), 
             DB::raw("CONCAT(finanzas.cantidad, ' ', unidades.nombre) as cantidad_unidad"),
             DB::raw("CONCAT('$', FORMAT(finanzas.costo_unitario, 2)) as costo_unitario"),
-            DB::raw("CONCAT('$', FORMAT((@subtotal := (finanzas.costo_unitario * finanzas.cantidad)), 2)) as subtotal_total_mxn"),
+            DB::raw("CONCAT('$', FORMAT(((finanzas.costo_unitario * finanzas.cantidad)), 2)) as subtotal"),
             DB::raw("CONCAT(ivas.porcentaje, ' ', '%') as iva"),
-            DB::raw("CONCAT('$', FORMAT((@subtotal * ivas.porcentaje), 2)) AS total_mxn"),
-            DB::raw("CONCAT('$', FORMAT(finanzas.monto_a_pagar, 2)) as monto_a_pagar"),
+            DB::raw("CONCAT('$', FORMAT(((finanzas.costo_unitario * finanzas.cantidad) * ivas.porcentaje), 2)) AS total"),
+            DB::raw("CONCAT('$', FORMAT(finanzas.monto_a_pagar, 2)) as monto_pagar"),
             DB::raw("DATE_FORMAT(finanzas.fecha_de_pago, '%Y-%m-%d') as fecha_de_pago"),
             'finanzas.metodo_de_pago',
             DB::raw("if(finanzas.es_pagado = 0, 'Pagado', 'Pendiente') as estatus"),
-            'finanzas.entregado_material_a',
+            'finanzas.entregado_material_a as entregado',
             DB::raw("if(finanzas.a_meses is not NULL, (SELECT GROUP_CONCAT(if(DATE_FORMAT(fac.mes_de_pago, '%Y-%m') = DATE_FORMAT(NOW(), '%Y-%m'), if(fac.monto is not NULL and fac.monto != 0, CONCAT('<span class=\"badge bg-success\">', DATE_FORMAT(fac.mes_de_pago, '%Y-%m'), ' ', 'Pagado</span>'), CONCAT('<span class=\"badge bg-warning text-dark\">', DATE_FORMAT(fac.mes_de_pago, '%Y-%m'), ' ', 'Por Vencer</span>')), if(fac.mes_de_pago < NOW(), if(fac.monto is not NULL and fac.monto != 0, '', CONCAT('<span class=\"badge bg-danger\">', DATE_FORMAT(fac.mes_de_pago, '%Y-%m'), ' ', 'Vencido</span>')), ''))) FROM facturas as fac WHERE fac.finanza_id = finanzas.id), 'N/A') as a_meses"),
             DB::raw("DATE_FORMAT(finanzas.fecha_facturacion, '%Y-%m-%d') as fecha_facturacion"),
             'finanzas.comentario',
@@ -94,44 +93,84 @@ class FinanzaTabla extends Component
         ->leftJoin('categorias_familias', 'categorias_familias.id', '=', 'finanzas.categoria_id')
         ->leftJoin('familias', 'familias.id', '=', 'categorias_familias.id')
         ->when($this->no, function ($query) {
-            $query->where('finanzas.no', $this->no);
+            $query->having('no', 'like', '%' . $this->no . '%');
         })
         ->when($this->fecha_entrada, function ($query) {
-            $query->where('finanzas.fecha_entrada', 'like', '%' . $this->fecha_entrada . '%');
+            $query->having('fecha_entrada', 'like', '%' . $this->fecha_entrada . '%');
         })
         ->when($this->fecha_salida, function ($query) {
-            $query->where('finanzas.fecha_salida', 'like', '%' . $this->fecha_salida . '%');
+            $query->having('fecha_salida', 'like', '%' . $this->fecha_salida . '%');
         })
         ->when($this->vence, function ($query) {
-            $query->where('finanzas.vence', 'like', '%' . $this->vence . '%');
+            $query->having('vence', 'like', '%' . $this->vence . '%');
         })
         ->when($this->fecha_vencimiento, function ($query) {
-            $query->where('finanzas.fecha_vencimiento', 'like', '%' . $this->fecha_vencimiento . '%');
+            $query->having('fecha_vencimiento', 'like', '%' . $this->fecha_vencimiento . '%');
         })
         ->when($this->dias, function ($query) {
-            $query->whereRaw("DATEDIFF(DATE_FORMAT(DATE_ADD(finanzas.fecha_salida, INTERVAL finanzas.vence DAY), '%Y-%m-%d'), NOW()) LIKE '%".$this->dias."%'");
+            $query->having('dias', 'like', '%' . $this->dias . '%');
         })
         ->when($this->estado, function ($query) {
-            $query->whereRaw("IF(DATEDIFF(DATE_FORMAT(DATE_ADD(finanzas.fecha_salida, INTERVAL finanzas.vence DAY), '%Y-%m-%d'), NOW()) <= 0, 'Vencido', 'Por Vencer') LIKE '%".$this->estado."%'");
+            $query->having('estado', 'like', '%' . $this->estado . '%');
         })
         ->when($this->tipo, function ($query) {
-            $query->whereRaw("if(finanzas.entradas_id is not NULL, 'Egreso', 'Ingreso') LIKE '%".$this->tipo."%'");
+            $query->having('tipo', 'like', '%' . $this->tipo . '%');
         })
         ->when($this->fam_cat, function ($query) {
-            $query->whereRaw("CONCAT(IF(familias.nombre IS NULL, '', CONCAT('F:', familias.nombre)), ', ', IF(categorias_familias.nombre IS NULL, '', CONCAT('F:', categorias_familias.nombre))) LIKE '%".$this->fam_cat."%'");
+            $query->having('fam_cat', 'like', '%' . $this->fam_cat . '%');
         })
         ->when($this->razon_social, function ($query) {
-            $query->whereRaw("if(finanzas.salidas_id is not NULL, proveedores.razon_social, clientes.razon_social) LIKE '%".$this->razon_social."%'");
+            $query->having('razon_social', 'like', '%' . $this->razon_social . '%');
         })
         ->when($this->proyecto, function ($query) {
-            $query->where('proyectos.nombre', 'like', '%' . $this->proyecto . '%');
+            $query->having('proyecto', 'like', '%' . $this->proyecto . '%');
         })
         ->when($this->descripcion, function ($query) {
-            $query->where('finanzas.descripcion', 'like', '%' . $this->descripcion . '%');
+            $query->having('descripcion', 'like', '%' . $this->descripcion . '%');
         })
         ->when($this->factura_folio, function ($query) {
-            $query->whereRaw("(SELECT COALESCE(GROUP_CONCAT(fac.referencia_factura),if(finanzas.entradas_id is not NULL, 'No Facturado', 'No Recibido')) FROM facturas as fac WHERE fac.finanza_id = finanzas.id) LIKE '%".$this->factura_folio."%'");
+            $query->having('fac_o_fol', 'like', '%' . $this->factura_folio . '%');
         })
+        ->when($this->provedor_cliente, function ($query) {
+            $query->having('provedor_cliente', 'like', '%' . $this->provedor_cliente . '%');
+        })
+        ->when($this->cantidad_unidad, function ($query) {
+            $query->having('cantidad_unidad', 'like', '%' . $this->cantidad_unidad . '%');
+        })
+        ->when($this->costo_unitario, function ($query) {
+            $query->having('costo_unitario', 'like', '%' . $this->costo_unitario . '%');
+        })
+        ->when($this->subtotal, function ($query) {
+            $query->having('subtotal', 'like', '%' . $this->subtotal . '%');
+        })
+        ->when($this->iva, function ($query) {
+            $query->having('iva', 'like', '%' . $this->iva . '%');
+        })
+        ->when($this->total, function ($query) {
+            $query->having('total', 'like', '%' . $this->total . '%');
+        })
+        ->when($this->monto_pagar, function ($query) {
+            $query->having('monto_pagar', 'like', '%' . $this->monto_pagar . '%');
+        })
+        ->when($this->monto_pagar, function ($query) {
+            $query->having('monto_pagar', 'like', '%' . $this->monto_pagar . '%');
+        })
+        ->when($this->estatus, function ($query) {
+            $query->having('estatus', 'like', '%' . $this->estatus . '%');
+        })
+        ->when($this->entregado, function ($query) {
+            $query->having('entregado', 'like', '%' . $this->entregado . '%');
+        })
+        ->when($this->a_meses, function ($query) {
+            $query->having('a_meses', 'like', '%' . $this->a_meses . '%');
+        })
+        ->when($this->comentario, function ($query) {
+            $query->having('comentario', 'like', '%' . $this->comentario . '%');
+        })
+        ->when($this->comprobante, function ($query) {
+            $query->having('comprobante', 'like', '%' . $this->comprobante . '%');
+        })
+        
         // ->orderBy('finanzas.' . $this->orderBy, $this->orderAsc ? 'asc' : 'desc')
         // ->toSql();
         // dd($finanzas);
--- resources/views/livewire/finanza-tabla.blade.php
+++ resources/views/livewire/finanza-tabla.blade.php
@@ -27,6 +27,9 @@
     .form-control[readonly]{
         background-color: transparent;
     }
+    .completo {
+        white-space: normal !important;
+    }
     </style>    
     <div class="row d-flex align-items-center size">
         <div class="col-sm-1" style="width: 175px;">
@@ -69,93 +72,122 @@
         <thead>
             <tr class="filters">
                 <th class="px-1 py-1">
+                    <span class="invisible">ooooooooooooooo</span>
                     <input wire:model="no" type="text" class="form-control" placeholder="No">
                 </th>
                 <th class="px-1 py-1">
+                    <span class="invisible">ooooooooooooooo</span>
                     <input wire:model="fecha_entrada" type="text" class="form-control" placeholder="Fecha Entrada">
                 </th>
                 <th class="px-1 py-1">
-                    <input wire:model="fecha_salida" type="text" class="form-control" placeholder="Fecha Entrada">
+                    <span class="invisible">ooooooooooooooo</span>
+                    <input wire:model="fecha_salida" type="text" class="form-control" placeholder="Fecha Salida">
                 </th>
                 <th class="px-1 py-1">
-                    <input wire:model="vence" type="text" class="form-control" placeholder="Fecha Entrada">
+                    <span class="invisible">ooooooooooooooo</span>
+                    <input wire:model="vence" type="text" class="form-control" placeholder="Vence">
                 </th>
 
                 <th class="px-1 py-1">
+                    <span class="invisible">ooooooooooooooo</span>
                     <input wire:model="fecha_vencimiento" type="text" class="form-control" placeholder="fecha Vencimiento">
                 </th>
                 <th class="px-1 py-1">
+                    <span class="invisible">ooooooooooooooo</span>
                     <input wire:model="dias" type="text" class="form-control" placeholder="Días">
                 </th>
                 <th class="px-1 py-1">
+                    <span class="invisible">ooooooooooooooo</span>
                     <input wire:model="estado" type="text" class="form-control" placeholder="Estado">
                 </th>
                 <th class="px-1 py-1">
+                    <span class="invisible">ooooooooooooooo</span>
                     <input wire:model="tipo" type="text" class="form-control" placeholder="Tipo I&E">
                 </th>
                 <th class="px-1 py-1">
+                    <span class="invisible">ooooooooooooooo</span>
                     <input wire:model="fam_cat" type="text" class="form-control" placeholder="Fam & Cat">
                 </th>
                 <th class="px-1 py-1">
+                    <span class="invisible">ooooooooooooooo</span>
                     <input wire:model="razon_social" type="text" class="form-control" placeholder="Razón social">
                 </th>
                 <th class="px-1 py-1">
+                    <span class="invisible">ooooooooooooooo</span>
                     <input wire:model="proyecto" type="text" class="form-control" placeholder="Proyecto">
                 </th>
                 <th class="px-1 py-1">
+                    <span class="invisible">ooooooooooooooo</span>
                     <input wire:model="descripcion" type="text" class="form-control" placeholder="Descripción">
                 </th>
                 <th class="px-1 py-1">
+                    <span class="invisible">ooooooooooooooo</span>
                     <input wire:model="factura_folio" type="text" class="form-control" placeholder="Factura o Folios">
                 </th>
                 <th class="px-1 py-1">
+                    <span class="invisible">ooooooooooooooo</span>
                     <input wire:model="provedor_cliente" type="text" class="form-control" placeholder="Proveedor o cliente">
                 </th>
                 <th class="px-1 py-1">
+                    <span class="invisible">ooooooooooooooo</span>
                     <input wire:model="cantidad_unidad" type="text" class="form-control" placeholder="Cantidad & Unidad">
                 </th>
                 <th class="px-1 py-1">
+                    <span class="invisible">ooooooooooooooo</span>
                     <input wire:model="costo_unitario" type="text" class="form-control" placeholder="Costo Unitario">
                 </th>
                 <th class="px-1 py-1">
+                    <span class="invisible">ooooooooooooooo</span>
                     <input wire:model="subtotal" type="text" class="form-control" placeholder="Subtotal MXN">
                 </th>
                 <th class="px-1 py-1">
+                    <span class="invisible">ooooooooooooooo</span>
                     <input wire:model="iva" type="text" class="form-control" placeholder="IVA">
                 </th>
                 <th class="px-1 py-1">
+                    <span class="invisible">ooooooooooooooo</span>
                     <input wire:model="total" type="text" class="form-control" placeholder="Total $MXN$">
                 </th>
                 <th class="px-1 py-1">
+                    <span class="invisible">ooooooooooooooo</span>
                     <input wire:model="monto_pagar" type="text" class="form-control" placeholder="Monto A Pagar">
                 </th>
                 <th class="px-1 py-1">
+                    <span class="invisible">ooooooooooooooo</span>
                     <input wire:model="fecha_pago" type="text" class="form-control" placeholder="Fecha De Pago">
                 </th>
                 <th class="px-1 py-1">
+                    <span class="invisible">ooooooooooooooo</span>
                     <input wire:model="metodo_pago" type="text" class="form-control" placeholder="Metodo De Pago">
                 </th>
                 <th class="px-1 py-1">
+                    <span class="invisible">ooooooooooooooo</span>
                     <input wire:model="estatus" type="text" class="form-control" placeholder="$ Estatus $">
                 </th>
                 <th class="px-1 py-1">
+                    <span class="invisible">ooooooooooooooo</span>
                     <input wire:model="entregado" type="text" class="form-control" placeholder="Entregado Material A">
                 </th>
                 <th class="px-1 py-1">
+                    <span class="invisible">ooooooooooooooo</span>
                     <input wire:model="a_meses" type="text" class="form-control" placeholder="A Meses">
                 </th>
                 <th class="px-1 py-1">
+                    <span class="invisible">ooooooooooooooo</span>
                     <input wire:model="fecha_facturacion" type="text" class="form-control" placeholder="Fecha Facturacion">
                 </th>
                 <th class="px-1 py-1">
+                    <span class="invisible">ooooooooooooooo</span>
                     <input wire:model="comentario" type="text" class="form-control" placeholder="Comentario">
                 </th>
                 <th class="px-1 py-1">
+                    <span class="invisible">ooooooooooooooo</span>
                     <input wire:model="comprobante" type="text" class="form-control" placeholder="Comprobante">
                 </th>
                 <th>Fecha Actualización</th>
 
                 <th>Acciones</th>
+                <th>Acc. Especiales</th>
             </tr>
         </thead>
         <tbody>
@@ -191,8 +223,8 @@
                 <td>{{ $item->costo_unitario }}</td>
                 <td>{{ $item->subtotal_total_mxn }}</td>
                 <td>{{ $item->iva }}</td>
-                <td>{{ $item->total_mxn }}</td>
-                <td>{{ $item->monto_a_pagar }}</td>
+                <td>{{ $item->total }}</td>
+                <td>{{ $item->monto_pagar }}</td>
                 <td>{{ $item->fecha_de_pago }}</td>
                 <td>{{ $item->metodo_de_pago }}</td>
                 <td>
@@ -202,7 +234,7 @@
                         <p class="badge bg-danger">Pendiente</p>
                     @endif
                 </td>
-                <td>{{ $item->entregado_material_a }}</td>
+                <td>{{ $item->entregado }}</td>
                 <td>{{ $item->a_meses }}</td>
                 <td>{{ $item->fecha_facturacion }}</td>
                 <td>{{ $item->comentario }}</td>
@@ -218,36 +250,39 @@
                 <td>
                     <span class="completo">
                         <form action="{{ route('finanzas.destroy',$item->id) }}" method="POST">
-                            @can('finanzas.confirmarpago')
-                            <a class="btn btn-sm btn-info" href="{{ route('finanzas.confirmarPago',$item->id) }}"><i class="fa fa-fw fa-eye"></i> Actualizar Pago</a>      
-                            @endcan
-                            @if ($item->salidas_id)
-                            @can('finanzas.correo')
-                            <a class="btn btn-sm btn-secondary " href="{{ route('finanzas.correo',$item->id) }}"><i class="fa fa-fw fa-eye"></i> Correo</a>      
-                            @endcan
-                            @endif
-                            @can('facturas.index')    
-                            <a class="btn btn-sm btn-warning" href="{{ route('facturas.facturafinanzas', ['id' => $item->id]) }}"><i class="fa fa-fw fa-edit"></i> Factura</a>
-                            @endcan
+                           
                             @can('finanzas.show')
-                            <a class="btn btn-sm btn-primary " href="{{ route('finanzas.show',$item->id) }}"><i class="fa fa-fw fa-eye"></i> Mostrar</a>
+                                <a class="btn btn-sm btn-primary " href="{{ route('finanzas.show',$item->id) }}"><i class="fa fa-fw fa-eye"></i></a>
                             @endcan
                             @can('finanzas.edit')
                                 @if (!empty($item->a_meses))
-                                    <a class="btn btn-sm btn-success" href="{{ route('finanzas.editEgresoMeses',$item->id) }}"><i class="fa fa-fw fa-edit"></i>Editar</a>
+                                    <a class="btn btn-sm btn-success" href="{{ route('finanzas.editEgresoMeses',$item->id) }}"><i class="fa fa-fw fa-edit"></i></a>
                                 @elseif($item->salidas_id)
-                                    <a class="btn btn-sm btn-success" href="{{ route('finanzas.editEgreso',$item->id) }}"><i class="fa fa-fw fa-edit"></i>Editar</a>
+                                    <a class="btn btn-sm btn-success" href="{{ route('finanzas.editEgreso',$item->id) }}"><i class="fa fa-fw fa-edit"></i></a>
                                 @else
-                                    <a class="btn btn-sm btn-success" href="{{ route('finanzas.editIngreso',$item->id) }}"><i class="fa fa-fw fa-edit"></i>Editar</a>
+                                    <a class="btn btn-sm btn-success" href="{{ route('finanzas.editIngreso',$item->id) }}"><i class="fa fa-fw fa-edit"></i></a>
                                 @endif
                             @endcan
                             @csrf
                             @method('DELETE')
                             @can('finanzas.destroy')
-                            <button type="submit" class="btn btn-danger btn-sm show_confirm"><i class="fa fa-fw fa-trash"></i> Borrar</button>
+                            <button type="submit" class="btn btn-danger btn-sm show_confirm"><i class="fa fa-fw fa-trash"></i></button>
                             @endcan
                         </form>
                     </span>
+                </td>
+                <td>
+                    @can('finanzas.confirmarpago')
+                        <a class="btn btn-sm btn-info" href="{{ route('finanzas.confirmarPago',$item->id) }}"><i class="fas fa-credit-card"></i> Actualizar Pago</a>      
+                    @endcan
+                    @if ($item->salidas_id)
+                        @can('finanzas.correo')
+                            <a class="btn btn-sm btn-secondary " href="{{ route('finanzas.correo',$item->id) }}"><i class="fas fa-envelope"></i> Correo</a>      
+                        @endcan
+                    @endif
+                    @can('facturas.index')    
+                        <a class="btn btn-sm btn-warning" href="{{ route('facturas.facturafinanzas', ['id' => $item->id]) }}"><i class="fas fa-file-invoice"></i> Factura</a>
+                    @endcan
                 </td>
             </tr>
             @endforeach
