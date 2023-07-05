95730a14aea7926efcb7c9f8cc75c9e821cfb232
CR-MX <ciat117@gmail.com>
2023-07-04 21:27:34 -0600

    fix: ahora si funciona update en rol

--- app/Http/Controllers/RoleController.php
+++ app/Http/Controllers/RoleController.php
@@ -93,15 +93,10 @@ class RoleController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, Roles $role)
     {   
-        $request->validate([
-            'name' => 'required'
-        ]); 
-
-        $rol = Role::findById($id);
-        $rol->update($request->all());
-        $rol->permissions()->sync($request->permissions);
+        $role->update($request->all());
+        $role->permissions()->sync($request->input('permissions', []));
         
         return redirect()->route('roles.index')
             ->with('success', 'Rol actualizado exitosamente.');
