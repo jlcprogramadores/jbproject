a7b3a73ea787a31a303217acd9932b8c4c457e7a
joseluisreyesm <48143883+joseluisreyesm@users.noreply.github.com>
2023-07-14 19:47:10 -0600

    403 custom
    .

---
+++ resources/views/errors/403.blade.php
@@ -0,0 +1,9 @@
+@extends('adminlte::page')
+@section('content')
+    <div class="lock"></div>
+        <div class="message">
+            <br>
+            <h1>Error 403: No tienes acceso a este recurso</h1>
+            <p>Por favor revisa con el administrador si crees que se trata de un error.</p>
+    </div>
+@endsection
