4bc20c12c8cd40882825641abe08845af4316ef8
CR-MX <ciat117@gmail.com>
2023-07-22 12:10:23 -0600

    feat: se implementaron los mensajes de alerta

--- resources/views/historial-alta/index.blade.php
+++ resources/views/historial-alta/index.blade.php
@@ -26,8 +26,11 @@
                         </div>
                     </div>
                     @if ($message = Session::get('success'))
-                        <div class="alert alert-success">
-                            <p>{{ $message }}</p>
+                        <div class="alert alert-success alert-dismissible fade show" role="alert">
+                            {{ $message }}
+                            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                              <span aria-hidden="true">&times;</span>
+                            </button>
                         </div>
                     @endif
 
