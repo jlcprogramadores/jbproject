c4f192923029b2fcae575614ac6ad7e8092202ab
CR-MX <ciat117@gmail.com>
2023-08-01 19:09:08 -0600

    fix: ahora se uso el login por defualt editable

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,73 +1,42 @@
-@extends('adminlte::page')
-@section('title','Inicio de Sesión')
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Inicio de Sesión') }}</div>
+@extends('adminlte::auth.auth-page')
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
-
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Correo electrónico:') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Contraseña:') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Recuérdame') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Iniciar Sesión') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Olvidaste tu contraseña') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
-                    </form>
+@section('auth_body')
+    <div class="auth-box">
+        <div class="auth-title">
+            <h5>Autenticarse para Iniciar sesión</h5>
+        </div>
+        <hr>
+        <form class="auth-form" method="POST" action="{{ route('login') }}">
+            @csrf
+            <div class="form-group input-group">
+                <input type="email" name="email" id="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" placeholder="Correo electrónico" required autofocus>
+                <div class="input-group-append">
+                    <span class="input-group-text">
+                        <i class="fas fa-envelope"></i>
+                    </span>
                 </div>
             </div>
+            <div class="form-group input-group">
+                <input type="password" name="password" id="password" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" placeholder="Contraseña" required>
+                <div class="input-group-append">
+                    <span class="input-group-text">
+                        <i class="fas fa-lock"></i>
+                    </span>
+                </div>
+                @if ($errors->has('email'))
+                    <span class="invalid-feedback" role="alert">
+                        <strong>Correo o Contraseña Invalido</strong>
+                    </span>
+                @endif
+            </div>
+            <div class="form-group form-check">
+                <input type="checkbox" class="form-check-input" name="remember" id="remember">
+                <label class="form-check-label" for="remember">Recuérdame</label>
+            </div>
+            <button type="submit" class="btn btn-primary btn-block">Iniciar sesión</button>
+        </form>
+        <div class="auth-links">
+            {{-- <a href="{{ route('password.request') }}">¿Olvidaste tu contraseña?</a> --}}
         </div>
     </div>
-</div>
 @endsection
