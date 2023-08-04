0f18b37ef7bbca37780e5de51deadc329b21125b
CR-MX <ciat117@gmail.com>
2023-08-03 21:26:55 -0600

    feat: avance Gastos proveedores

--- resources/views/finanza/gastosProveedores.blade.php
+++ resources/views/finanza/gastosProveedores.blade.php
@@ -1,87 +1,76 @@
 @extends('adminlte::page')
-
 @section('title', 'Gastos Proveedores')
-        @section('content')
-            <section class="content container-fluid">
-                <div class="row">
-                    <div class="col-md-12">
-                        <div class="card">
-                            <div class="card-header">
-                                <span class="card-title">Gastos Proveedores</span>
-                            </div>
-                            <div class="card-body">
-                                <form method="POST" action="{{ route('finanzas.tablaGastosProveedores') }}" role="form"
-                                    enctype="multipart/form-data">
-                                    @csrf
-                                    <div class="container">
-                                        <div class="row">
-                                            <div class="col-sm p-1 form-group ">
-                                                <label class="proyecto_id" name="Ingreso_id" for="Ingreso_id">Gastos de Proveedores por Proyecto:</label>
-                                                <br>
-                                                <select class="form-control" id="proyecto_id" name="proyecto_id">
-                                                    <option value="" selected="selected">Selecciona Proyecto</option>
-                                                    @foreach ($proyecto as $val => $name)
-                                                        <option value="{{ $val }}">{{ $name }}</option>
-                                                    @endforeach
-                                                </select>
-                                            </div>
-
-                                        </div>                                    
-                                        <br>
-                                        <div class="row justify-content-md-center">
-                                            <button id="btn_graficar" type="submit"
-                                                class="btn btn-primary col col-lg-3">Obtener Gastos</button>
-                                        </div>
-                                    </div>
+<style>
+    .select2-container--default .select2-selection--multiple .select2-selection__choice {
+        color: #000000 !important;
+    }
+</style>
+@section('content')
+    <section class="content container-fluid">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header">
+                        <span class="card-title">Gastos Proveedores</span>
+                    </div>
+                    <div class="card-body">
+                            <div class="row">
+                                <div class="col-2">
+                                    <label class="invisible">Acción</label>
+                                    <button type="button" id="btn_carga" onclick="cargaTabla();" class="btn btn-primary col">Carga Proyectos</button>
+                                </div>
+                                <div class="col-10 p-1 form-group ">
+                                    <label class="proyecto_id">Selecciona los proyectos a cargar:</label>
                                     <br>
-                                </form>
+                                    <select class="form-control" id="proyecto_id" name="proyectos[]" multiple="multiple">
+                                        <option value="" selected="selected">Todos los proyectos</option>
+                                        @foreach ($proyecto as $val => $name)
+                                            <option value="{{ $val }}">{{ $name }}</option>
+                                        @endforeach
+                                    </select>
+                                </div>
                             </div>
-                        </div>
+                            <span id="body_tabla">
+                                <div class="row">
+                                    <div class="table-responsive">
+                                        <table id="table" class="table table-striped table-hover" id="table">
+                                            <thead class="thead">
+                                                <tr>
+                                                    <th>Proveedores</th>
+                                                    <th>Pagado</th>
+                                                    <th>Pendiente Pagar</th>
+                                                    <th>Total General</th>
+                                                    
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+                                                {{-- @foreach ($collection as $item) --}}
+                                                <tr>
+                                                    <td></td>
+                                                    <td></td>
+                                                    <td></td>
+                                                    <td></td>
+                                                </tr>
+                                                    
+                                                {{-- @endforeach --}}
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                </div>
+                            </span>
                     </div>
                 </div>
-            </section>
-        @endsection
-        @push('js')
-            
-            <script>
-                $('#proyecto_id').select2();
-                let desdeAux = "";
-                let hastaAux = "";
-                document.getElementById("advertencia").style.display = "none";
-
-                function funcionDesde(val) {
-                    desdeAux = val;
-                    if (desdeAux > hastaAux && hastaAux != "") {
-                        document.getElementById("btn_filtrar").style.display = "none";
-                        document.getElementById("advertencia").style.display = "block";
-
-                    } else {
-                        document.getElementById("btn_filtrar").style.display = "block";
-                        document.getElementById("advertencia").style.display = "none";
-                    }
-                }
-
-                function funcionHasta(val) {
-                    hastaAux = val;
-                    if (desdeAux > hastaAux) {
-                        document.getElementById("btn_filtrar").style.display = "none";
-                        document.getElementById("advertencia").style.display = "block";
-                    } else {
-                        document.getElementById("btn_filtrar").style.display = "block";
-                        document.getElementById("advertencia").style.display = "none";
-                    }
-                }
-
-                $('#tipo').on('select2:select', function(e) {
-                    var data = e.params.data;
-                    console.log(data.id);
-                    if (data.id != 1) { // es ingreso
-                        document.getElementById("cliente").style.display = "block";
-                        document.getElementById("proveedor").style.display = "none";
-                    } else { // es egreso
-                        document.getElementById("proveedor").style.display = "block";
-                        document.getElementById("cliente").style.display = "none";
-                    }
-                });
-            </script>
-        @endpush
+            </div>
+        </div>
+    </section>
+@endsection
+@push('js')
+    <script>
+        $('#proyecto_id').select2();
+        function cargaTabla(){
+            let proyectos = $('#proyecto_id').val();
+            console.log(proyectos);
+        }
+        
+    </script>
+@endpush
